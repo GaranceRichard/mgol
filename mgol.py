296a89f7d4f88c378d5d611712daf218e4e13188
garance <garance.richard@gmail.com>
2017-12-15 22:45:33 +0100

    Amélioration de l'interface

--- mgol.py
+++ mgol.py
@@ -15,12 +15,15 @@ def sel():
 	window.destroy()
 
 text1 = Label(window,text="Paramètres de l'application :")
+text1.config(font=("monospace",15))
 scale1 = Scale(window, variable = screen, orient='horizontal', from_=500, to=1000,
       resolution=10, tickinterval=100, length=500,
-      label='Taille de l\'écran')
+      label='Taille de l\'écran :')
+scale1.config(font=("monospace",12))
 scale2 = Scale(window, variable = case, orient='horizontal', from_=10, to=100,
       resolution=1, tickinterval=10, length=500,
-      label='Cases par cotés')
+      label='Cases par cotés :')
+scale2.config(font=("monospace",12))
 button = Button(window, text="Valider", command = sel)
 text1.pack()
 scale1.pack()
