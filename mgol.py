1f8b128fa14a4841d4ff70530a8f0ca8a882fae3
garance <garance.richard@gmail.com>
2017-12-17 12:29:12 +0100

    Optimisation du code

--- mgol.py
+++ mgol.py
@@ -37,6 +37,7 @@ white = (255,255,255)
 black = (0,0,0)
 red = (255,0,0)
 green = (0,255,0)
+
 matrice = np.zeros((cases,cases))
 unit = screen_size/cases
 continuer = 1
@@ -50,7 +51,7 @@ def maj(screen):
 	label = myfont.render(str(repeat), 1, red)
 	label2 = myfont.render(str(etat_du_jeu), 1, black)
 	label2b = myfont.render("taille :", 1, green)
-	label3 = myfont.render(str(matrice.shape[0]**2), 1, green)
+	label3 = myfont.render(str(matrice.shape[0])+"x"+str(matrice.shape[0]), 1, green)
 	fenetre.blit(label0, (screen_size+10, 10))
 	fenetre.blit(label, (screen_size+10, 30))
 	fenetre.blit(label2, (screen_size+10, screen_size/2))
@@ -64,6 +65,23 @@ def maj(screen):
 					pygame.draw.rect(screen, red,(x*unit,y*unit,unit,unit),0)
 	pygame.display.update()
 
+
+def control(x,y):
+	#fonction de contrôle qui permet de compter l'entourage d'une cellule
+	try:
+		control_array = [0]*8	
+		control_array[0] = control_matrice.item((x-1,y+1))
+		control_array[1] = control_matrice.item((x,y+1))
+		control_array[2] = control_matrice.item((x+1,y+1))
+		control_array[3] = control_matrice.item((x-1,y))
+		control_array[4] = control_matrice.item((x+1,y))
+		control_array[5] = control_matrice.item((x-1,y-1))
+		control_array[6] = control_matrice.item((x,y-1))
+		control_array[7] = control_matrice.item((x+1,y-1))
+	except IndexError:
+		pass
+	return sum(control_array)
+
 pygame.init()
 fenetre = pygame.display.set_mode((int(screen_size+200),screen_size))
 pygame.display.set_caption('Jeu de la vie')
@@ -72,7 +90,7 @@ maj(fenetre)
 test = 1
 while test == 1:
 	while continuer == 1: # boucle d'initialisation du panel
-		repeat = 0
+		# repeat = 0
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				test = 0
@@ -101,7 +119,7 @@ while test == 1:
 					pygame.display.update()
 					pygame.time.wait(100)
 				except IndexError:
-					pygame.draw.rect(fenetre, green, (matrice.shape[0]*unit,0,(int(screen_size+150)-matrice.shape[0]),screen_size),0)
+					pygame.draw.rect(fenetre, green, (matrice.shape[0]*unit,0,(int(screen_size+200)-matrice.shape[0]),screen_size),0)
 					pygame.display.update()
 					pygame.time.wait(100)
 					continuer = 2
@@ -112,42 +130,21 @@ while test == 1:
 		control_matrice = np.pad(matrice,1,'constant') 	#on crée une matrice copie entourée de zéros
 		future_matrice = np.zeros((cases+2,cases+2))	#la matrice de retour des entourage
 		next_matrice = np.zeros((cases,cases))			#matrice générée
-		
-
-		def control(x,y):
-			#fonction de contrôle qui permet de compter l'entourage d'une cellule
-			try:
-				control_array = [0]*8	
-				control_array[0] = control_matrice.item((x-1,y+1))
-				control_array[1] = control_matrice.item((x,y+1))
-				control_array[2] = control_matrice.item((x+1,y+1))
-				control_array[3] = control_matrice.item((x-1,y))
-				control_array[4] = control_matrice.item((x+1,y))
-				control_array[5] = control_matrice.item((x-1,y-1))
-				control_array[6] = control_matrice.item((x,y-1))
-				control_array[7] = control_matrice.item((x+1,y-1))
-			except IndexError:
-				pass
-			return sum(control_array)
 
 		for x in range(control_matrice.shape[0]):
 				for y in range(control_matrice.shape[1]):
-					a=control(x,y)
-					future_matrice[(x,y)] = a
+					future_matrice[(x,y)] = control(x,y)
 
 		future_matrice = future_matrice[1:-1,1:-1]
 
 		for x in range(future_matrice.shape[0]):
 			for y in range(future_matrice.shape[1]):
-				if matrice.item((x,y)) ==  0 and future_matrice.item((x,y)) == 3:
+				if (future_matrice.item((x,y)) == 3) or (matrice.item((x,y)) ==  1 and (future_matrice.item((x,y)) == 2)):
 					next_matrice[(x,y)] = 1
-				if matrice.item((x,y)) ==  1 and (future_matrice.item((x,y)) == 3):
-					next_matrice[(x,y)] = 1
-				if matrice.item((x,y)) ==  1 and (future_matrice.item((x,y)) == 2):
-					next_matrice[(x,y)] = 1
-
 
 		if (matrice == next_matrice).all():
+			etat_du_jeu = "Lancer"
+			maj(fenetre)
 			continuer = 1
 		else:
 			matrice = next_matrice
