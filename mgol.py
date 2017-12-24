01c81451352c9c6b342ce199df4bfff05fbf9904
garance <garance.richard@gmail.com>
2017-12-24 16:26:59 +0100

    amélioration de l'affichage

--- mgol.py
+++ mgol.py
@@ -30,11 +30,15 @@ def maj(screen):
 	fenetre.blit(label2b, (screen_size+10, 70))
 	fenetre.blit(label3, (screen_size+10, 90))	
 	for x in range(matrice.shape[0]):
+		if matrice.shape[0]>100 and matrice[x].max()==0:
+			pass
+		else:
 			for y in range(matrice.shape[1]):
 				if matrice.item((x,y)) == 0 and matrice.shape[0]<=100:
 					pygame.draw.rect(screen, black,(x*unit,y*unit,unit,unit),1)
 				elif matrice.item((x,y)) == 1:
 					pygame.draw.rect(screen, red,(x*unit,y*unit,unit,unit),0)
+	pygame.draw.line(screen,black,(screen_size,0),(screen_size,screen_size))
 	pygame.display.update()
 
 
@@ -62,6 +66,9 @@ def click(button):
 	if button == 5:
 		matrice = np.pad(matrice,1,'constant')
 		cases = matrice.shape[0]
+	if button == 2:
+		transitoire = ((matrice*8)+np.random.randint(11,size=matrice.shape))>7
+		matrice = transitoire+0
 	if button == 1:
 		pos = pygame.mouse.get_pos()
 		pos = (np.asarray(pos)/(screen_size/matrice.shape[0])).astype(int)
@@ -77,6 +84,7 @@ def click(button):
 				continuer = 2
 			else:
 				continuer = 1
+
 	unit = screen_size/cases
 	maj(fenetre)
 	pygame.display.update()
@@ -88,6 +96,7 @@ maj(fenetre)
 
 test = 1
 while test == 1:
+	etat_du_jeu = "Lancer"
 	while continuer == 1: # boucle d'initialisation du panel
 		for event in pygame.event.get():
 			if event.type == QUIT:
@@ -105,8 +114,11 @@ while test == 1:
 		future_matrice = np.zeros((cases+2,cases+2))	#la matrice de retour des entourage
 		next_matrice = np.zeros((cases,cases))			#matrice générée
 
-		for x in range(control_matrice.shape[0]):
-				for y in range(control_matrice.shape[1]):
+		for x in range(1,control_matrice.shape[0]-1):
+			if (control_matrice[x-1].max() == 0) and (control_matrice[x].max() == 0) and (control_matrice[x+1].max() == 0):
+				pass
+			else:
+				for y in range(1,control_matrice.shape[1]-1):
 					future_matrice[(x,y)] = control(x,y)
 
 		future_matrice = future_matrice[1:-1,1:-1]
@@ -117,7 +129,6 @@ while test == 1:
 					next_matrice[(x,y)] = 1
 
 		if (matrice == next_matrice).all():
-			etat_du_jeu = "Lancer"
 			maj(fenetre)
 			continuer = 1
 		else:
